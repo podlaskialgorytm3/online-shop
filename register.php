9650dd8f3a867412779bc577d325e95894b81781
Michał Kozak <live.m851@gmail.com>
2023-09-22 21:58:51 +0200

    Create simple register form

---
+++ register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/register-form.css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+    <title>Register</title>
+</head>
+<body>
+<div class="container">
+        <h2>Zarejestruj się!</h2>
+        <form action="#" method="post">
+            <div class="form-group">
+                <input type="text" id="username" name="username" placeholder="Wpisz swoją nazwę użytkownika!">
+            </div>
+            <div class="form-group">
+                <input type="text" id="firstName" name="firstName" placeholder="Wpisz swoje imię!">
+            </div>
+            <div class="form-group">
+                <input type="text" id="lastName" name="lastName" placeholder="Wpisz swoje nazwisko!">
+            </div>
+            <div class="form-group">
+                <input type="text" id="address" name="address" placeholder="Wpisz swój adres zamieszkania!">
+            </div>
+            <div class="form-group">
+                <input type="email" id="email" name="email" placeholder="Wpisz swój email!">
+            </div>
+            <div class="form-group">
+                <input type="password" id="password" name="password" placeholder="Wpisz swoje hasło!">
+            </div>
+            <div class="form-group">
+                <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Powtórz hasło!">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Zarejestruj się">
+            </div>
+        </form>
+    </div>
+
+
+</body>
+</html>
