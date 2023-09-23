381097e1c265a936036ab78632f0f489548c6827
Michał Kozak <live.m851@gmail.com>
2023-09-23 17:28:39 +0200

    Create user page

---
+++ user_pages/user.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../components/meta.php' ?>
+    <title>SKLEP M&D | USER</title>
+</head>
+<body>
+    <?php include '../components/header.php' ?>
+    <h1>STRONA DLA USERA</h1>
+</body>
+</html>
