3bcded3be2c68c5d487bc49017fddee8f24f55c1
Michał Kozak <live.m851@gmail.com>
2023-09-23 16:14:37 +0200

    Create login page

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'components/meta.php' ?>
+    <title>SKLEP M&D | Logownaie</title>
+    <link rel="stylesheet" href="css/register-form.css">
+</head>
+<body>
+<div class="container">
+        <h2>Zaloguj się!</h2>
+        <form action="#" method="post">
+            <div class="form-group">
+                <input type="email" id="email" name="email" placeholder="Wpisz swój email!">
+            </div>
+            <div class="form-group">
+                <input type="password" id="password" name="password" placeholder="Wpisz swoje hasło!">
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Zaloguj się" style="font-size: 18px;">
+            </div>
+            <div class="form-group">
+                <p>Nie masz jeszcze konta? <a href="register.php">Zarejestruj się!</a></p>
+            </div>
+        </form>
+    </div>
+
+
+</body>
+</html>
