33dcf1b8e3cf14f0cac7d8af7d8860ed1f892349
Michał Kozak <live.m851@gmail.com>
2023-09-23 16:16:35 +0200

    Create file meta for meta and other links for section <head></head>

---
+++ components/meta.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/main.css">
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+<link rel="shortcut icon" href="images/shop_icon.png" type="image/x-icon">
