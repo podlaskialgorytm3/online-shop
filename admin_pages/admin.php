f083d93c2d24d735952e8549677e14ead8f370be
Michał Kozak <live.m851@gmail.com>
2023-09-23 16:33:20 +0200

    Create admin page

---
+++ admin_pages/admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include '../components/meta.php' ?>
+    <title>SKLEP M&D | ADMIN</title>
+</head>
+<body>
+    <?php include '../components/header.php' ?>
+    <h1>STRONA DLA ADMINISTRATORA</h1>
+</body>
+</html>
