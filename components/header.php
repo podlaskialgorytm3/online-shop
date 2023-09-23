60e1bf2b2d6826f2935e6e86d7d3f3e542a60d09
Michał Kozak <live.m851@gmail.com>
2023-09-23 16:12:41 +0200

    Create a header section and styling it

---
+++ components/header.php
@@ -0,0 +1,14 @@
+<div id="header">
+    <div class="header-left-side">
+        <img src="images/shop_icon.png" alt="Icon our shop!" style="width: 80px">            
+    </div>
+    <div class="header-right-side">
+        <div class="shoping-cart">
+        <!-- <img src="images/shopping_cart.png" alt="Shopping cart." style="width: 60px">  -->
+        </div>
+        <div class="accounts">
+            <a href="login.php">Zaloguj się!</a>
+            <a href="register.php">Zarejestruj się!</a>
+        </div>
+    </div>
+</div>
