ba48f2861372646a8a9de29f587d775c8b6d9c3a
Michał Kozak <live.m851@gmail.com>
2023-09-24 13:42:40 +0200

    [MAIL] - Working on sending token to mail.

---
+++ forgot-password.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'components/meta.php' ?>
+    <title>SKLEP M&D | Przypomnij hasło!</title>
+    <link rel="stylesheet" href="css/register-form.css">
+</head>
+<body>
+<div class="container">
+    <h2>Przypomij hasło!</h2>
+    <form action="vendor/send-password-reset.php" method="post">
+        <div class="form-group">
+            <input type="email" id="email" name="email" placeholder="Wpisz swój email!">
+        </div>
+        <div class="form-group">
+                <button class="send-code">Wyślij kod</button>
+        </div>
+    </form>
+</body>
+</html>
