59f2b10b8534597036b012d9339a3c1f513c1534
Dawid <dawid.zmudzki@wp.pl>
2023-09-25 19:37:49 +0200

    some updates

--- process-reset-password.php
+++ process-reset-password.php
@@ -43,4 +43,20 @@ $stmt->bind_param("ss",$password,$user['ID_USER']);
 
 $stmt->execute();
 
-echo "Hasło zostało zmienione!";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SKLEP M&D</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/register-form.css">
+</head>
+<body>
+<div class="container">
+    <div class="form-group">
+        <h2>Hasło zostało zmienione!</h2>
+        <p>Chcesz się zalogować? <a href="/login.php">Zaloguj się!</a></p>
+    </div>
+</div>
+</body>
+</html>
