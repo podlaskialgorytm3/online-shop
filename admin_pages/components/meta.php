172af6c45c25434b66ac761387ec59cf09d64348
Michał Kozak <live.m851@gmail.com>
2023-09-29 23:48:06 +0200

    some fix

--- admin_pages/components/meta.php
+++ admin_pages/components/meta.php
@@ -3,4 +3,4 @@
 <link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="../../css/main.css">
 <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
-<link rel="shortcut icon" href="images/shop_icon.png" type="image/x-icon">
+<link rel="shortcut icon" href="../../images/shop_icon.png" type="image/x-icon">
