6eca4bafd5128493d6b3f8496df25d5ab4d15d6d
Michał Kozak <live.m851@gmail.com>
2023-09-29 23:47:11 +0200

    Add others pages to admin manage system

--- admin_pages/components/header.php
+++ admin_pages/components/header.php
@@ -8,7 +8,9 @@
     </div>
     <div class="header-right-side">
         <div class="management-menu">
-            <a href="../../admin_pages/order-menage.php" title="Zarządzanie zamówieniami"><img src="../../images/manage_order.png" alt="Zarządzanie zamówieniami"></a>
+        <a href="../../admin_pages/order-menage.php" title="Zarządzanie zamówieniami"><img src="../../images/manage_order.png" alt="Zarządzanie zamówieniami"></a>
+            <a href="../../admin_pages/delivery-menage.php" title="Zarzązanie dostawami"><img src="../../images/menage-delivery.png" alt="Zarządzanie dostawami"></a>
+            <a href="../../admin_pages/payment-menage.php" title="Zarządzanie płatności"><img src="../../images/menage-payment.png" alt="Zarządzanie płatności"></a>
             <a href="../../admin_pages/client-manage.php" title="Zarzązanie klientami"><img src="../../images/manage_client.png" alt="Zarządzanie klientami"></a>
             <a href="../../admin_pages/product-menage.php" title="Zarządzanie produktami"><img src="../../images/product.png" alt="Zarządzanie produktami"></a>
             <a href="../../logout.php" title="Wyloguj się!"><img src="../../images/logout.png" alt="Wyloguj się!"></a>
---
+++ admin_pages/payment-menage.php
@@ -0,0 +1,21 @@
+<?php 
+@include '../config.php';
+session_start();
+if(!isset($_SESSION['user_admin'])){
+    header('location:login.php');
+ }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php @include '../admin_pages/components/meta.php' ?>
+    <title>Zarządzanie Płatnościami | SKLEP M&D</title>
+</head>
+<body>
+<?php @include "components/header.php" ?>
+    <h1>STRONA DLA ADMINISTRATORA | ZARZĄDZANIE PŁATNOŚCIAMI</h1>
+    <h3>Witaj <?php echo $_SESSION['user_admin']; ?></h3>
+</body>
+</html>
