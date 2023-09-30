35af1ab979c2d135c09f76ba4f8e0377e1e1457a
Michał Kozak <live.m851@gmail.com>
2023-09-30 22:36:59 +0200

    Some changes of constuction admin panel

--- admin_pages/components/header.php
+++ admin_pages/components/header.php
@@ -2,18 +2,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../../css/header.css">
 </head>
-<div id="header">
-    <div class="header-left-side">
-        <a href="../../admin_pages/admin.php"><img src="../images/shop_icon.png" alt="Icon our shop!" style="width: 80px"></a>            
-    </div>
-    <div class="header-right-side">
-        <div class="management-menu">
-        <a href="../../admin_pages/order-menage.php" title="Zarządzanie zamówieniami"><img src="../../images/manage_order.png" alt="Zarządzanie zamówieniami"></a>
-            <a href="../../admin_pages/delivery-menage.php" title="Zarzązanie dostawami"><img src="../../images/menage-delivery.png" alt="Zarządzanie dostawami"></a>
-            <a href="../../admin_pages/payment-menage.php" title="Zarządzanie płatności"><img src="../../images/menage-payment.png" alt="Zarządzanie płatności"></a>
-            <a href="../../admin_pages/client-manage.php" title="Zarzązanie klientami"><img src="../../images/manage_client.png" alt="Zarządzanie klientami"></a>
-            <a href="../../admin_pages/product-menage.php" title="Zarządzanie produktami"><img src="../../images/product.png" alt="Zarządzanie produktami"></a>
-            <a href="../../logout.php" title="Wyloguj się!"><img src="../../images/logout.png" alt="Wyloguj się!"></a>
-        </div>
-    </div>
+<div class="management-menu">
+    <a href="../../logout.php" title="Wyloguj się!"><img src="../../images/logout.png" alt="Wyloguj się!"></a>
 </div>
