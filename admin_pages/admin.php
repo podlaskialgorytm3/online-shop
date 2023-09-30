dc6fe95892495950dc2e613f70b9487bc9f63dd2
Michał Kozak <live.m851@gmail.com>
2023-09-30 23:16:34 +0200

    Create side panel to admin dashboard and design

--- admin_pages/admin.php
+++ admin_pages/admin.php
@@ -14,6 +14,7 @@ if(!isset($_SESSION['user_admin'])){
 </head>
 <body>
     <?php @include "components/header.php" ?>
+    <?php @include '../admin_pages/components/side-panel.php' ?>
     <h1>STRONA DLA ADMINISTRATORA</h1>
     <h3>Witaj <?php echo $_SESSION['user_admin']; ?></h3>
 </body>
---
+++ admin_pages/components/side-panel.php
@@ -0,0 +1,19 @@
+<head>
+    <link rel="stylesheet" href="../../css/side-panel.css">
+</head>
+<div class="side-panel">
+    <div class="title-shop">
+        <img src="../../images/shop_icon.png" alt="Ikona sklepu"><p>SKLEP M&D</p>
+    </div>
+    <div class="main-panel">
+        <a href="../../admin_pages/admin.php">Panel Admina</a>
+    </div>
+    <div class="manage-panel">
+        <h2>Zarządzanie</h2>
+        <a href="../../admin_pages/product-menage.php">Produkty</a>
+        <a href="../../admin_pages/client-manage.php">Klienci</a>
+        <a href="../../admin_pages/order-menage.php">Zamówienia</a>
+        <a href="../../admin_pages/delivery-menage.php">Dostawy</a>
+        <a href="../../admin_pages/payment-menage.php">Płatności</a>
+    </div>
+</div>
