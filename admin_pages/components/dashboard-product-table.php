78f6b24c5608ac6cd49977880f7e306833baf32d
Michał Kozak <live.m851@gmail.com>
2023-10-04 21:49:51 +0200

    Styling popup for adding category of product

--- admin_pages/components/dashboard-product-table.php
+++ admin_pages/components/dashboard-product-table.php
@@ -54,14 +54,27 @@
         +
     </div>
 </div>
-<div class="add-product-panel">
+<div class="add-product-panel add-panel">
     
 </div>
-<div class="add-tag-panel">
+<div class="add-tag-panel add-panel">
     
 </div>
-<div class="add-category-panel">
-    
+<div class="add-category-panel add-panel">
+    <form action="" method="POST">
+        <h2>Dodaj Kategorię</h2>
+        <label for="nazwa_kategorii">
+            <input type="text" name="nazwa_kategorii" placeholder="Wpisz nazwę kategorii.">
+            <span class="name-category-error error-alert"></span>
+        </label>
+        <label for="opis_kategorii">
+            <input type="text" name="opis_kategorii" placeholder="Wpisz opis kategorii.">
+            <span class="description-category-error error-alert"></span>
+        </label>
+        <label for="submit">
+            <input type="submit" name="submit" value="Dodaj Kategorie">
+        </label>
+    </form>
 </div>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="../../js/ChangesProductTable.js"></script>
