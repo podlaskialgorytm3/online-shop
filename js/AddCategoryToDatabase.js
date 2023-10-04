83c77e700cd2cad5a2f7580e41b6b110b9baca5e
Michał Kozak <live.m851@gmail.com>
2023-10-04 22:55:34 +0200

    Create open pop-up and validation

---
+++ js/AddCategoryToDatabase.js
@@ -0,0 +1,46 @@
+const addCategoryBtn = document.querySelector(".add-category")
+const addCategoryPanel = document.querySelector(".add-category-panel")
+const sideBar = document.querySelector(".side-panel")
+const dashboard = document.querySelector(".dashboard")
+
+const nameCategory = document.querySelector(".name-category")
+const descriptionCategory = document.querySelector(".description-category")
+const submit = document.querySelector(".category-submit")
+const nameCategoryError = document.querySelector(".name-category-error")
+const descriptionCategoryError = document.querySelector(".description-category-error")
+
+
+
+
+addCategoryBtn.addEventListener("click",() => {
+    addCategoryPanel.style.display = "flex";
+    dashboard.style.display = "none";
+    sideBar.style.display = "none"
+})
+
+submit.addEventListener("click",(e) => {
+    e.preventDefault()
+    stepValidation = 0
+    if(nameCategory.value == ""){
+        nameCategoryError.textContent = "Pole jest wymagane!"
+    }
+    else{
+        stepValidation++
+        nameCategoryError.textContent = ""
+    }
+    if(descriptionCategory.value == ""){
+        descriptionCategoryError.textContent = "Pole jest wymagane!"
+    }
+    else{
+        stepValidation++
+        descriptionCategoryError.textContent = ""
+    }
+    if(stepValidation == 2){
+        addData()
+    }
+})
+
+
+addData = () => {
+
+}
