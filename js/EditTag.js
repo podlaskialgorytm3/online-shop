e6bdd7ac1dc3cfa9dfa267a44b51d1b786038328
Michał Kozak <live.m851@gmail.com>
2023-10-08 15:08:02 +0200

    Getting date to edit tags

---
+++ js/EditTag.js
@@ -0,0 +1,10 @@
+editButtonTag = document.querySelectorAll(".edit-tag")
+
+nameTagEdit = document.querySelector(".name-tag-edit")
+descriptionTagEdit = document.querySelector(".description-tag-edit")
+nameTagErrorEdit = document.querySelector(".name-tag-error-edit")
+descriptionTagErrorEdit = document.querySelector(".description-tag-error-edit")
+submitTagEdit = document.querySelector(".tag-edit-submit")
+exitEditTag = document.querySelector(".exit-edit-tag")
+editTagPanel = document.querySelector(".edit-tag-panel")
+
