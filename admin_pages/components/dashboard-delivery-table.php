168e90984226a214cc2e6f9b36489dc95a7fb360
Michał Kozak <live.m851@gmail.com>
2023-10-13 21:03:23 +0200

    Create a dashboard to view delivery options

---
+++ admin_pages/components/dashboard-delivery-table.php
@@ -0,0 +1,19 @@
+<head>
+ <link rel="stylesheet" href="../../css/dashboard.css">
+ <link rel="stylesheet" href="../../css/product-tables.css">
+</head>
+<div class="dashboard">
+<table class="user-table table">
+    <thead>
+            <th>Id_dostawcy</th>
+            <th>Nazwa</th>
+            <th>Adres</th>
+            <th>Email</th>
+            <th>Cena</th>
+            <th>Usuń</th>
+            <th>Edytuj</th>
+    </thead>
+    <tbody id="delivery-body">
+    </tbody>
+</table>
+</div>
