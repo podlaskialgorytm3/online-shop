533b7b1fc476df8bec95437161721244039b6d2d
Michał Kozak <live.m851@gmail.com>
2023-10-19 23:28:44 +0200

    Getting data to js

--- admin_pages/components/dashboard-create-page.php
+++ admin_pages/components/dashboard-create-page.php
@@ -19,5 +19,36 @@
         +
 </div>
 </div>
+
+
+<div class="add-page-panel add-panel">
+    <div class="exit exit-add-page"><img src="../../images/x.png" alt=""></div>
+    <form action="" method="POST">
+        <h2>Dodaj Stronę</h2>
+        <label for="Tytuł">
+            <input type="text" name="tytul_strony" placeholder="Wpisz tytuł strony." class="title-page">
+            <span class="title-page-error error-alert"></span>
+        </label>
+        <label for="Opis">
+            <textarea name="opis_strony" class="description-page" cols="30" rows="10" placeholder="Wpisz opis strony"></textarea>
+            <span class="description-page-error error-alert"></span>
+        </label>
+        <label for="Status">
+            <select name="" class="status-page">
+                <option value="widoczny">widoczny</option>
+                <option value="ukryty">ukryty</option>
+            </select>
+        </label>
+        <label for="submit">
+            <input type="submit" name="submit" value="Edytuj Dostawcę" class="page-submit">
+        </label>
+    </form>
+</div>
+<div class="success-add">
+    <img src="../../images/checked.png" alt=""><p class="success-text"></p>
+</div>
+
+
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="../../js/GetPage.js"></script>
+<script src="../../js/AddPageToDatabase.js"></script>
---
+++ js/AddPageToDatabase.js
@@ -0,0 +1,15 @@
+const dashboard = document.querySelector(".dashboard")
+const sideBar = document.querySelector(".side-panel")
+const addPage = document.querySelector(".add-page")
+const exitPanel  = document.querySelector(".exit-add-page")
+const pageSubmit = document.querySelector(".page-submit")
+
+const titlePage = document.querySelector(".title-page")
+const descriptionPage = document.querySelector(".description-page")
+const statusPage = document.querySelector(".status-page")
+
+const titleErrorPage = document.querySelector(".title-page-error")
+const descriptionErrorPage = document.querySelector(".description-page-error")
+
+const successAdd = document.querySelector(".success-add")
+const successText = document.querySelector(".success-text")
