5954b0af72f62f92cd03cf2507e1305ec2d3acc6
Michał Kozak <live.m851@gmail.com>
2023-10-20 19:35:56 +0200

    Turn on/off add panel

--- js/AddPageToDatabase.js
+++ js/AddPageToDatabase.js
@@ -3,6 +3,7 @@ const sideBar = document.querySelector(".side-panel")
 const addPage = document.querySelector(".add-page")
 const exitPanel  = document.querySelector(".exit-add-page")
 const pageSubmit = document.querySelector(".page-submit")
+const addPagePanel = document.querySelector(".add-page-panel")
 
 const titlePage = document.querySelector(".title-page")
 const descriptionPage = document.querySelector(".description-page")
@@ -13,3 +14,22 @@ const descriptionErrorPage = document.querySelector(".description-page-error")
 
 const successAdd = document.querySelector(".success-add")
 const successText = document.querySelector(".success-text")
+
+
+const showPagePanel = () => {
+    dashboard.style.display = "none"
+    sideBar.style.display = "none"
+    addPagePanel.style.display = "flex"
+}
+const exitPagePanel = () => {
+    dashboard.style.display = "block"
+    sideBar.style.display = "block"
+    addPagePanel.style.display = "none"
+}
+addPage.addEventListener("click",() => {
+    showPagePanel()
+
+})
+exitPanel.addEventListener("click",() => {
+    exitPagePanel()
+})
