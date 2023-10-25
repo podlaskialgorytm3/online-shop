456943c70a0a0ac87b7245e4b7b80d3842eda1eb
Michał Kozak <live.m851@gmail.com>
2023-10-25 20:17:42 +0200

    Add elements to main admin panel

--- admin_pages/components/dashboard-admin.php
+++ admin_pages/components/dashboard-admin.php
@@ -1,9 +1,19 @@
 <head>
  <link rel="stylesheet" href="../../css/dashboard.css">
- <link rel="stylesheet" href="../../css/product-tables.css">
+ <link rel="stylesheet" href="../../css/main-admin.css">
 </head>
 <div class="dashboard">
-    <h1>STRONA DLA ADMINISTRATORA</h1>
-    <h3>Witaj <?php echo $_SESSION['user_admin']; ?></h3>
+    <div class="admin-information">
+        <div class="header-info">
+            <img src="../../images/admin-img.png" alt="" class="admin-img">
+            <h2>nick: <h2 class="nick"></h2></h2>
+        </div>
+        <p><b>Imię: </b><p class="name"></p></p>
+        <p><b>Nazwisko </b><p class="surname"></p></p>
+        <p><b>Adres e-mail: </b><p class="email"></p></p>
+        <p><b>Adres zamieszaknia: </b><p class="address"></p></p>
+        <p><b>Notatka: </b><p class="note"></p></p>
+    </div>
 </div>
+<input type="hidden" value="<?php echo $_SESSION['user_admin']; ?>">
 <script src="../../js/GetDataToAdmin.js"></script>
