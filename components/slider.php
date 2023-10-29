f7ffc1c5f040ab3239ae34a9e417acdbf147222a
Michał Kozak <live.m851@gmail.com>
2023-10-29 12:23:46 +0100

    Export slider to one other file

---
+++ components/slider.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.core.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.core.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.theme.min.css">
+    <link rel="stylesheet" href="../css/slider.css">
+</head>
+<body>
+<div class="glide">
+    <div class="glide__track" data-glide-el="track">
+        <ul class="glide__slides">
+            <li class="glide__slide slide1"></li>
+            <li class="glide__slide slide2"></li>
+            <li class="glide__slide slide3"></li>
+        </ul>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
+<script src="../js/Slider.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,24 +2,10 @@
 <html lang="en">
 <head>
     <?php include 'components/meta.php'?>
-    <link rel="stylesheet" href="node_modules/@glidejs/glide/dist/css/glide.core.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.core.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.theme.min.css">
-    <link rel="stylesheet" href="/css/slider.css">
     <title>SKLEP M&D</title>
 </head>
 <body>
     <?php include 'components/header.php' ?>
-    <div class="glide">
-    <div class="glide__track" data-glide-el="track">
-        <ul class="glide__slides">
-            <li class="glide__slide slide1"></li>
-            <li class="glide__slide slide2"></li>
-            <li class="glide__slide slide3"></li>
-        </ul>
-    </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
-<script src="js/Slider.js"></script>
+    <?php include 'components/slider.php' ?>
 </body>
 </html>
