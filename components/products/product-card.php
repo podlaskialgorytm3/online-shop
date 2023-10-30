b9ae6203980f854283129df0da52e42e103a22a5
Michał Kozak <live.m851@gmail.com>
2023-10-30 22:22:48 +0100

    Create a construction of product card

---
+++ components/products/product-card.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    <div class="product-card">
+        <div class="product-image">
+
+        </div>
+        <div class="product-info">
+            <h2 class="product-title">
+
+            </h2>
+            <p class="product-description">
+
+            </p>
+            <div class="footer">
+                <p class="price"></p>
+                <button class="add-to-cart">DODAJ DO KOSZYKA</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
