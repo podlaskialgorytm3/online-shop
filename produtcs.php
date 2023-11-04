1c37835c3dc64a45c7a0581f0e0936118f383f14
Michał Kozak <live.m851@gmail.com>
2023-11-04 19:04:11 +0100

    Add search input to products page

--- produtcs.php
+++ produtcs.php
@@ -2,10 +2,17 @@
 <html lang="en">
 <head>
     <?php include 'components/meta.php'?>
+    <link rel="stylesheet" href="/css/search.css">
     <title>Produkty</title>
 </head>
 <body>
     <?php include 'components/header.php' ?>
+    <div class="container">
+        <input type="text" placeholder="Search...">
+        <div class="search"></div>
+    </div>
+    <div class="main">
     <?php include 'guest_pages/product-container.php'?>
+    </div>
 </body>
 </html>
