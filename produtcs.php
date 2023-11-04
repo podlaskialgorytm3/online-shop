bcdd695370dfd5d185d75acb71b1aef4b069fcbc
Michał Kozak <live.m851@gmail.com>
2023-11-04 18:47:42 +0100

    Create a page with all products

---
+++ produtcs.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'components/meta.php'?>
+    <title>Produkty</title>
+</head>
+<body>
+    <?php include 'components/header.php' ?>
+    <?php include 'guest_pages/product-container.php'?>
+</body>
+</html>
