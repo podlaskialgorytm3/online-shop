588d0bfc535e3f5bf8aac40bacba34fdf42d95c6
Michał Kozak <live.m851@gmail.com>
2023-11-06 16:56:24 +0100

    Seasoning file with getting local storage

--- js/HandleShopCart.js
+++ js/HandleShopCart.js
@@ -1,11 +1,12 @@
 const size = document.querySelector(".sizes")
 const color = document.querySelector(".colors")
-const button = document.querySelector(".add-to-card")
+const buttonCard = document.querySelector(".add-to-card")
 
 let nameValue = ""
 let priceValue = ""
+let cart = JSON.parse(localStorage.getItem('cart1')) || [];
 
-const getProductInfo = (button, color, size) => {
+const getProductInfo = (buttonCard, color, size) => {
     return new Promise((resolve, reject) => {
         const ajax = new XMLHttpRequest();
         ajax.open("GET", "../components/fetch-product.php", true);
@@ -14,13 +15,14 @@ const getProductInfo = (button, color, size) => {
             if (this.readyState == 4 && this.status == 200) {
                 let database = JSON.parse(this.responseText);
                 for (let i = 0; i < database.length; i++) {
-                    if (database[i].Id_produktu == button.dataset.id) {
+                    if (database[i].Id_produktu == buttonCard.dataset.id) {
                         let productInformation = {
-                            id: button.dataset.id,
+                            id: buttonCard.dataset.id,
                             name: database[i].nazwa_produktu,
                             price: database[i].cena,
                             color: color.value,
                             size: size.value,
+                            URL: database[i].URL
                         };
                         resolve(productInformation);
                     }
@@ -30,28 +32,17 @@ const getProductInfo = (button, color, size) => {
     });
 };
 
-button.addEventListener("click", () => {
-    getProductInfo(button, color, size).then((productInfo) => {
+buttonCard.addEventListener("click", () => {
+    getProductInfo(buttonCard, color, size).then((productInfo) => {
         if(typeof Storage !== 'undifinded'){
-            let cart = JSON.parse(localStorage.getItem('cart')) || []
+            let cart = JSON.parse(localStorage.getItem('cart1')) || []
             cart.push(productInfo)
-            localStorage.setItem('cart', JSON.stringify(cart));
+            localStorage.setItem('cart1', JSON.stringify(cart));
         }
         else{
 
         }
-        let cart = JSON.parse(localStorage.getItem('cart')) || [];
-        cart.forEach(function (cart_item) {
-        console.log("Produkt: " + cart_item.name);
-        console.log("Cena: " + cart_item.price);
-        console.log("Color: " + cart_item.color);
-        console.log("Rozmiar: " + cart_item.size);
-});
-
     }).catch((error) => {
         console.error("Wystąpił błąd: ", error);
     });
 });
-
-
-
