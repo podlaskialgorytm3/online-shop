ba5ee64f7a90b1d8e25aec8603b035daf73a4c14
Michał Kozak <live.m851@gmail.com>
2023-11-06 17:04:21 +0100

    Show simple data in shop cart

--- js/ShoppingCart.js
+++ js/ShoppingCart.js
@@ -1,6 +1,7 @@
 const exit = document.querySelector(".exit-shop-cart")
 const entry = document.querySelector(".entry-shop-cart")
 const shoppingCart = document.querySelector(".shopping-cart")
+const items = document.querySelector("#items")
 
 exit.addEventListener("click",() => {
     shoppingCart.style.display = "none"
@@ -8,3 +9,10 @@ exit.addEventListener("click",() => {
 entry.addEventListener("click",() => {
     shoppingCart.style.display = "flex"
 })
+let cart1 = JSON.parse(localStorage.getItem('cart1')) || []
+
+cart1.forEach(cartItem => {
+    let div = document.createElement("div")
+    div.innerHTML = `<div><p>${cartItem.name}</p><p>${cartItem.price} zł</p><p>${cartItem.color}</p><p>${cartItem.size}</p></div>`
+    items.appendChild(div)
+});
