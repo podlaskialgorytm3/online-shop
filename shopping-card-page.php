f11cdeea8661632bd165611db7d601e714b951b0
Michał Kozak <live.m851@gmail.com>
2023-11-06 20:02:28 +0100

    Create page for shopping cart to modify your order

---
+++ shopping-card-page.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include __DIR__ . '/components/meta.php' ?>
+    <title>Koszyk</title>
+</head>
+<body>
+    <?php include __DIR__ . '/components/header.php' ?>
+</body>
+</html>
