f93e9a9d2b5218fa67b82801d48c2e70014c4de2
Michał Kozak <live.m851@gmail.com>
2023-11-06 18:19:43 +0100

    Dynamic adding product to shoping cart

--- js/HandleShopCart.js
+++ js/HandleShopCart.js
@@ -32,12 +32,52 @@ const getProductInfo = (buttonCard, color, size) => {
     });
 };
 
+const getTagsName = (id) => {
+    return new Promise((resolve, reject) => {
+        const ajax = new XMLHttpRequest();
+        ajax.open("GET", "../components/fetch-tag.php", true);
+        ajax.send();
+        ajax.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                let database = JSON.parse(this.responseText);
+                for (let i = 0; i < database.length; i++) {
+                    if (database[i].id_parametru == id) {
+                        resolve(database[i].wartosc_parametru);
+                        return;
+                    }
+                }
+                resolve("Brak parametru.");
+            }
+        };
+    });
+}
+
 buttonCard.addEventListener("click", () => {
     getProductInfo(buttonCard, color, size).then((productInfo) => {
         if(typeof Storage !== 'undifinded'){
-            let cart = JSON.parse(localStorage.getItem('cart1')) || []
+            let cart = JSON.parse(localStorage.getItem('cart2')) || []
             cart.push(productInfo)
-            localStorage.setItem('cart1', JSON.stringify(cart));
+            localStorage.setItem('cart2', JSON.stringify(cart));
+            console.log(cart)
+            let div = document.createElement("div")
+            getTagsName(cart[cart.length -1].color).then(colorName => {
+            getTagsName(cart[cart.length -1].size).then(sizeName => {
+                div.innerHTML = `
+                <div class="container">
+                    <h2 class="product-name">${cart[cart.length -1].name}</h2>
+                    <div class="center-container">
+                        <div class="image" style="background-image: url(${cart[cart.length -1].URL})"></div>
+                        <div class="infomration-container">
+                            <p><b>Cena: </b>${cart[cart.length -1].price} zł</p>
+                            <p><b>Kolor: </b>${colorName}</p>
+                            <p><b>Rozmiar: </b>${sizeName}</p>
+                        </div>
+                    </div>
+                </div>
+                `
+                items.appendChild(div);
+            });
+        });
         }
         else{
 
--- js/ShoppingCart.js
+++ js/ShoppingCart.js
@@ -9,48 +9,53 @@ exit.addEventListener("click",() => {
 entry.addEventListener("click",() => {
     shoppingCart.style.display = "flex"
 })
-let cart1 = JSON.parse(localStorage.getItem('cart1')) || []
+let cart1 = JSON.parse(localStorage.getItem('cart2')) || []
 
-const getTagsName = (id) => {
-    return new Promise((resolve, reject) => {
-        const ajax = new XMLHttpRequest();
-        ajax.open("GET", "../components/fetch-tag.php", true);
-        ajax.send();
-        ajax.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                let database = JSON.parse(this.responseText);
-                for (let i = 0; i < database.length; i++) {
-                    if (database[i].id_parametru == id) {
-                        resolve(database[i].wartosc_parametru);
-                        return;
+const addItemToShopCart = () => {
+    while (items.firstChild) {
+        items.removeChild(items.firstChild);
+    }
+    const getTagsName = (id) => {
+        return new Promise((resolve, reject) => {
+            const ajax = new XMLHttpRequest();
+            ajax.open("GET", "../components/fetch-tag.php", true);
+            ajax.send();
+            ajax.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    let database = JSON.parse(this.responseText);
+                    for (let i = 0; i < database.length; i++) {
+                        if (database[i].id_parametru == id) {
+                            resolve(database[i].wartosc_parametru);
+                            return;
+                        }
                     }
+                    resolve("Brak parametru.");
                 }
-                resolve("Brak parametru.");
-            }
-        };
-    });
-}
-
-cart1.forEach(cartItem => {
-    let div = document.createElement("div")
-    getTagsName(cartItem.color).then(colorName => {
-        getTagsName(cartItem.size).then(sizeName => {
-            div.innerHTML = `
-            <div class="container">
-                <h2 class="product-name">${cartItem.name}</h2>
-                <div class="center-container">
-                    <div class="image" style="background-image: url(${cartItem.URL})"></div>
-                    <div class="infomration-container">
-                        <p><b>Cena: </b>${cartItem.price} zł</p>
-                        <p><b>Kolor: </b>${colorName}</p>
-                        <p><b>Rozmiar: </b>${sizeName}</p>
+            };
+        });
+    }
+    cart1.forEach(cartItem => {
+        let div = document.createElement("div")
+        getTagsName(cartItem.color).then(colorName => {
+            getTagsName(cartItem.size).then(sizeName => {
+                div.innerHTML = `
+                <div class="container">
+                    <h2 class="product-name">${cartItem.name}</h2>
+                    <div class="center-container">
+                        <div class="image" style="background-image: url(${cartItem.URL})"></div>
+                        <div class="infomration-container">
+                            <p><b>Cena: </b>${cartItem.price} zł</p>
+                            <p><b>Kolor: </b>${colorName}</p>
+                            <p><b>Rozmiar: </b>${sizeName}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            `
-            items.appendChild(div);
+                `
+                items.appendChild(div);
+            });
         });
     });
-});
-
-console.log(cart1)
+}
+document.addEventListener("DOMContentLoaded", () => {
+    addItemToShopCart()
+})
