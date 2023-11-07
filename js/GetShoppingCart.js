6c8aca3b171738e9d53b889f42128cb2d34d559f
Michał Kozak <live.m851@gmail.com>
2023-11-07 17:59:24 +0100

    Dynamic removing product in shop cart

--- js/GetShoppingCart.js
+++ js/GetShoppingCart.js
@@ -27,45 +27,57 @@ const getTagsName = (id) => {
 // Deklaracja tablicy do przechowania wszystkich obietnic
 const promises = [];
 
-cart2.forEach((cartItem,index) => {
-    let div = document.createElement("div");
-    const colorPromise = getTagsName(cartItem.color);
-    const sizePromise = getTagsName(cartItem.size);
-
-    promises.push(Promise.all([colorPromise, sizePromise]).then(([colorName, sizeName]) => {
-        div.innerHTML = `
-        <div class="container" style="width: 1000px; height: 350px;">
-            <h2 class="product-name product-name-scp">${cartItem.name}</h2>
-            <div class="center-container">
-                <div class="image" style="background-image: url(${cartItem.URL}); height: 300px;"></div>
-                <div class="infomration-container">
-                    <p><b>Cena: </b>${cartItem.price} zł</p>
-                    <p><b>Kolor: </b>${colorName}</p>
-                    <p><b>Rozmiar: </b>${sizeName}</p>
-                    <p><b>Ilość: </b>${cartItem.quanity}</p>
-                    <button data-id=${index} style="margin-top: 20px; cursor: pointer;" class="delete-product"><img src="../images/delete.png" style="width: 50px"/></button>
+const getShoppingCart = () => {
+    while (itemContainer.firstChild) {
+        itemContainer.removeChild(itemContainer.firstChild); // Usuń wszystkie dzieci rodzica
+    }
+    cart2.forEach((cartItem,index) => {
+        let div = document.createElement("div");
+        const colorPromise = getTagsName(cartItem.color);
+        const sizePromise = getTagsName(cartItem.size);
+        promises.push(Promise.all([colorPromise, sizePromise]).then(([colorName, sizeName]) => {
+            div.innerHTML = `
+            <div class="container" style="width: 1000px; height: 350px;">
+                <h2 class="product-name product-name-scp">${cartItem.name}</h2>
+                <div class="center-container">
+                    <div class="image" style="background-image: url(${cartItem.URL}); height: 300px;"></div>
+                    <div class="infomration-container">
+                        <p><b>Cena: </b>${cartItem.price} zł</p>
+                        <p><b>Kolor: </b>${colorName}</p>
+                        <p><b>Rozmiar: </b>${sizeName}</p>
+                        <p><b>Ilość: </b>${cartItem.quanity}</p>
+                        <button data-id=${index} style="margin-top: 20px; cursor: pointer;" class="delete-product"><img src="../images/delete.png" style="width: 50px"/></button>
+                    </div>
                 </div>
             </div>
-        </div>
-        `;
-        itemContainer.appendChild(div);
-    }));
-});
+            `;
+            itemContainer.appendChild(div);
+        }));
+    });
+    Promise.all(promises).then(() => {
+        const deleteButton = document.querySelectorAll(".delete-product");
+        deleteButton.forEach(btn => {
+            btn.addEventListener("click", () => {
+                console.log(btn.dataset.id)
+                let index = btn.dataset.id
+                if (index !== -1) {
+                 // Usuń produkt z koszyka
+                cart2.splice(index, 1);
+                console.log(cart2)
+                // Zapisz zaktualizowany koszyk z powrotem do Local Storage
+                localStorage.setItem('cart5', JSON.stringify(cart2));
+                getShoppingCart()
+            }
+            })
+        })
+    });
+}
+
+
 
 // Po zakończeniu wszystkich operacji asynchronicznych ustawiamy deleteButton
-Promise.all(promises).then(() => {
-    const deleteButton = document.querySelectorAll(".delete-product");
-    deleteButton.forEach(btn => {
-        btn.addEventListener("click", () => {
-            console.log(btn.dataset.id)
-            let index = btn.dataset.id
-            if (index !== -1) {
-             // Usuń produkt z koszyka
-            cart2.splice(index, 1);
-            console.log(cart2)
-            // Zapisz zaktualizowany koszyk z powrotem do Local Storage
-            localStorage.setItem('cart5', JSON.stringify(cart2));
-        }
-        })
-    })
-});
+
+
+document.addEventListener("DOMContentLoaded",() => {
+    getShoppingCart()
+})
