640cb131ef3b40628da1cd29b4f3d90eab5282b7
Michał Kozak <live.m851@gmail.com>
2023-11-07 18:43:35 +0100

    Create delivery page

---
+++ delivery-page.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include __DIR__ . '/components/meta.php' ?>
+    <title>Dostawa</title>
+</head>
+<body>
+    <?php include __DIR__ . '/components/header.php' ?>
+    
+</body>
+</html>
