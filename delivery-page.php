27ef1832394bc36285c1fc5d0a5601b57acb4ae6
Michał Kozak <live.m851@gmail.com>
2023-11-08 20:48:44 +0100

    Getting elements from html structure to js

--- delivery-page.php
+++ delivery-page.php
@@ -14,10 +14,14 @@
         </div>
         <div class="user-data-option">
             <h2>Podaj swoje dane.</h2>
-            <input type="text" placeholder="Podaj imię i nazwisko.">
-            <input type="text" placeholder="Podaj swój adres zamieszkania.">
-            <input type="email" placeholder="Podaj swój adres e-mail.">
-            <input type="number" placeholder="Podaj swój numer telefonu.">
+            <input type="text" placeholder="Podaj imię i nazwisko." class="name">
+            <span class="name-error error"></span>
+            <input type="text" placeholder="Podaj swój adres zamieszkania." class="address">
+            <span class="address-error error"></span>
+            <input type="email" placeholder="Podaj swój adres e-mail." class="email">
+            <span class="email-error error"></span>
+            <input type="number" placeholder="Podaj swój numer telefonu." class="number">
+            <span class="number-error error"></span>
         </div>
         <div class="payment-option">
 
--- js/GetDeliveryOptions.js
+++ js/GetDeliveryOptions.js
@@ -1,31 +1,39 @@
+const name = document.querySelector(".name")
+const address = document.querySelector(".address")
+const email = document.querySelector(".email")
+const number = document.querySelector(".number")
+
+const nameError = document.querySelector(".name-error")
+const addressError = document.querySelector(".address-error")
+const emailError = document.querySelector(".email-error")
+const numberError = document.querySelector(".number-error")
+
+
+
 function getDeliveryOptions(){
     $.ajax({
         url: "/guest_pages/get_data/get-delivery-option.php",
         method: 'POST'
     }).done(function( data ) {
         $('.delivery-option').html(data);
-        
-    })
-}
-function getPaymentOptions(){
-    $.ajax({
-        url: "/guest_pages/get_data/get-payment-option.php",
-        method: 'POST'
-    }).done(function( data ) {
-        $('.payment-option').html(data);
-        
+        $.ajax({
+            url: "/guest_pages/get_data/get-payment-option.php",
+            method: 'POST'
+        }).done(function( data ) {
+            $('.payment-option').html(data);
+            getPaymentId(data)
+            getDeliveryId(data)
+        })
     })
 }
 $(document).ready(function(){
     getDeliveryOptions();
-    getPaymentOptions();
 });
-
 const getDeliveryId = (data) =>{
     const deliveryInputs = document.querySelectorAll("input[name=delivery]")
     deliveryInputs.forEach(input => {
         input.addEventListener("click",() => {
-            return input.value
+            console.log(input.value)
         })
     })
 }
@@ -33,7 +41,7 @@ const getPaymentId = (data) => {
     const paymentInputs = document.querySelectorAll("input[name=payment]")
     paymentInputs.forEach(input => {
         input.addEventListener("click",() => {
-            return input.value
+            console.log(input.value)
         })
     })
 }
