b83d0d80c983fe2faa8432f50acc1cb8cfb3f896
Michał Kozak <live.m851@gmail.com>
2023-11-09 18:26:07 +0100

    Create summary page and add necessary data to page

--- summary-page.php
+++ summary-page.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php include __DIR__ . '/components/meta.php' ?>
+    <title>Podsumowanie zamówienia.</title>
 </head>
 <body>
-    
+    <?php include __DIR__ . '/components/bookmark.php' ?>
 
-<script>
-    console.log(JSON.parse(localStorage.getItem('user-data')))
-</script>
+
 </body>
 </html>
