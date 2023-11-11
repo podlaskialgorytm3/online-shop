acca5ecb966985709ce23697d43a3df5ce6b584e
Michał Kozak <live.m851@gmail.com>
2023-11-11 22:00:28 +0100

    Create structure of adding users type of worker

--- admin_pages/components/dashboard-client-table.php
+++ admin_pages/components/dashboard-client-table.php
@@ -58,6 +58,46 @@
         </label>
     </form>
 </div>
+
+<div class="add-user-panel add-panel">
+    <div class="exit exit-add-user"><img src="../../images/x.png" alt=""></div>
+    <form action="" method="POST">
+        <h2>Dodaj Użytkownika</h2>
+        <label for="Nazwa Użytkownika">
+            <input type="text" name="username" placeholder="Wpisz nazwę użytkownika." class="username-add">
+            <span class="username-error-add error-alert"></span>
+        </label>
+        <label for="Imię">
+            <input type="text" name="imie" placeholder="Wpisz imię." class="name-add">
+            <span class="name-error-add error-alert"></span>
+        </label>
+        <label for="Nazwisko">
+            <input type="text" name="nazwisko" placeholder="Wpisz nazwisko." class="lastname-add">
+            <span class="lastname-error-add error-alert"></span>
+        </label>
+        <label for="adres">
+            <input type="text" name="adres" placeholder="Wpisz adres zamieszkania." class="address-add">
+            <span class="address-error-add error-alert"></span>
+        </label>
+        <label for="email">
+            <input type="text" name="email" placeholder="Wpisz email." class="email-add">
+            <span class="email-error-add error-alert"></span>
+        </label>
+        <label for="typ">
+            <select name="typ" class="type-add">
+                <option value="worker">Pracownik</option>
+            </select>
+        </label>
+        <label for="notatka">
+            <input type="text" name="notatka" placeholder="Wpisz notatkę użytkownikowi." class="note-add">
+            <span class="note-error-add error-alert"></span>
+        </label>
+        <label for="submit">
+            <input type="submit" name="submit" value="Dodaj użytkownika" class="user-submit-add">
+        </label>
+    </form>
+</div>
+
 <div class="success-add success-add-category">
     <img src="../../images/checked.png" alt=""><p class="success-text"></p>
 </div>
