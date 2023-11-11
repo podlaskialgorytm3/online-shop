bb437866b296a01a83385cd7e81eb3b2faf19240
Michał Kozak <live.m851@gmail.com>
2023-11-11 22:26:23 +0100

    Creating validation when creating a user

--- js/AddUsersToDatabase.js
+++ js/AddUsersToDatabase.js
@@ -1,9 +1,9 @@
 const addUserModal = document.querySelector(".add-user-panel")
-const dashboard = document.querySelector(".dashboard")
 const sidePanel = document.querySelector(".side-panel")
 
 const addWorkerBtn = document.querySelector(".add-worker")
 const exit = document.querySelector(".exit-add-user")
+const userSubmitAdd = document.querySelector(".user-submit-add")
 
 const username = document.querySelector(".username-add")
 const name = document.querySelector(".name-add")
@@ -30,6 +30,73 @@ const turnOffModal = () => {
     dashboard.style.display = "block"
     sidePanel.style.display = "block"
 }
+let emails = []
+let currentEmail = ""
+ajax = new XMLHttpRequest();
+    ajax.open("GET","../components/fetch-data.php" , true);
+    ajax.send();
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let database = JSON.parse(this.responseText);
+            database.forEach(element =>{
+                emails.push(element['email'])
+            })
+    }
+}
+const validation = () => {
+    let stepValidation = 0
+    if(username.value == ""){
+        usernameError.textContent = "Wprowadzono puste pole!"
+    }
+    else{
+        usernameError.textContent = ""
+        stepValidation++
+    }
+    if(name.value == ""){
+        nameError.textContent = "Wprowadzono puste pole!"
+    }
+    else{
+        nameError.textContent = ""
+        stepValidation++
+    }
+    if(lastname.value == ""){
+        lastnameError.textContent = "Wprowadzono puste pole!"
+    }
+    else{
+        lastnameError.textContent = ""
+        stepValidation++
+    }
+    if(address.value == ""){
+        addressError.textContent = "Wprowadzono puste pole!"
+    }
+    else{
+        addressError.textContent = ""
+        stepValidation++
+    }
+    if(email.value == ""){
+        console.log(email.value)
+        emailError.textContent = "Wprowadzono puste pole!"
+    }
+    else{
+        mailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if(email.value.match(mailRegexp)){
+            if(emails.includes(email.value) && currentEmail != email.value){
+                emailError.textContent = "Podano już istniejący email!"
+            }
+            else{
+                emailError.textContent = ""
+                stepValidation++
+            }
+        }
+        else{
+            emailError.textContent = "Zły format e-mail!"
+        }
+    }
+    if(stepValidation == 5){
+        console.log("Poprawno zwalidowano!")
+    }
+}
+
 
 addWorkerBtn.addEventListener("click",() => {
     turnOnModal()
@@ -38,3 +105,9 @@ addWorkerBtn.addEventListener("click",() => {
 exit.addEventListener("click",() => {
     turnOffModal()
 })
+userSubmitAdd.addEventListener("click",(e) => {
+    e.preventDefault()
+    validation()
+})
+
+
