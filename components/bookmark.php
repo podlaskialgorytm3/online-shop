ca202fec99852fad34035be95c83bb653b7b7174
Michał Kozak <live.m851@gmail.com>
2023-11-14 19:05:08 +0100

    Add dynamic bookmark is dependency of session

--- components/bookmark.php
+++ components/bookmark.php
@@ -1,9 +1,17 @@
+<?php 
+@include '../config.php';
+$link = "../index.php";
+if(isset($_SESSION['user_name'])){
+    $link = "../user_pages/user.php";
+}
+
+?>
 <head>
     <link rel="stylesheet" href="../css/bookmark.css">
 </head>
 <body>
     <div class="bookmark">
-        <a href="index.php" title="Powrót na stronę główną."><img src="../images/shop_icon.png" alt="Ikonka sklepu!"></a>
+        <a href=<?php echo $link; ?> title="Powrót na stronę główną."><img src="../images/shop_icon.png" alt="Ikonka sklepu!"></a>
     </div>
 </body>
 </html>
