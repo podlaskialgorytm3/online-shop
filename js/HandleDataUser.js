a5f519fec037512034422930393c34780cf0ca19
Michał Kozak <live.m851@gmail.com>
2023-11-18 17:54:43 +0100

    Change file to get data from database

--- js/HandleDataUser.js
+++ js/HandleDataUser.js
@@ -5,13 +5,13 @@ const number = document.querySelector(".number")
 
 const getID = () => {
     return new Promise((resolve, reject) => {
-        var xmlhttp = new XMLHttpRequest();
-        var url = "../user_pages/get_data/get-id.php";
+        let xmlhttp = new XMLHttpRequest();
+        let url = "../user_pages/get_data/get-id.php";
         
         xmlhttp.onreadystatechange = function () {
             if (this.readyState == 4) {
                 if (this.status == 200) {
-                    var data = JSON.parse(this.responseText);
+                    let data = JSON.parse(this.responseText);
                     resolve(data);
                 } else {
                     reject(new Error('Request failed with status ' + this.status));
@@ -26,14 +26,14 @@ const getID = () => {
 const suplmenetingUserData = async () =>{
     let id = await getID()
     const ajax = new XMLHttpRequest();
-    ajax.open("GET","../components/fetch-data.php" , true);
+    ajax.open("GET","../components/fetch-user-data.php" , true);
     ajax.send();
     ajax.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let database = JSON.parse(this.responseText);
             for(let i = 0; i < database.length; i++){
                 if(database[i].ID_USER == id){
-                    nameUser.value = `${database[i].imie} ${database[i].nazwisko}`
+                    nameUser.value = `${database[i].imie_nazwisko}`
                     address.value = `${database[i].adres}`
                     email.value = `${database[i].email}`
                 } 
