8089302590d73c8257635e82d868b5b2f864cc7b
Michał Kozak <live.m851@gmail.com>
2023-11-18 19:50:10 +0100

    Styled user settings panel

--- user_pages/components/settings-account-dashboard.php
+++ user_pages/components/settings-account-dashboard.php
@@ -1,26 +1,32 @@
 <head>
 <link rel="stylesheet" href="../../css/client-dashboard.css">
 <link rel="stylesheet" href="../../css/data-user.css">
+<link rel="stylesheet" href="../../css/settings-user.css">
 </head>
 <div class="client-dashboard">
-    <h2>Dane konta:</h2>
-    <div class="main-user-data">
+    <div class="settings-container">
+    <h2>Dane konta</h2>
+    <div class="main-user-data item-content">
+        <h4>Twoje dane</h4>
         <div class="main-content">
 
         </div>
         <button class="main-edit">Edytuj</button>
     </div>
-    <div class="email-user-data">
+    <div class="email-user-data item-content">
+        <h4>Email</h4>
         <div class="email-content">
 
         </div>
         <button class="email-edit">Edytuj</button>
     </div>
-    <div class="password-user-data">
+    <div class="password-user-data item-content">
         <div class="password-content">
-            <input type="text" value="••••••••" disabled>
+            <h4>Hasło</h4>
+            <input type="text" value="••••••••" disabled style="display:block; width: 250px;">
         </div>
         <button class="password-edit">Edytuj</button>
+    </div>
     </div>
 </div>
 <div class="success-add">
