fe71454b1e4d46ea920355721775f827a09cf6c4
Michał Kozak <live.m851@gmail.com>
2023-11-18 18:28:13 +0100

    Complete empty inupts to data from db about ud

--- js/GetDeliveryOptionsUser.js
+++ js/GetDeliveryOptionsUser.js
@@ -48,7 +48,48 @@ function getDeliveryOptions(){
 }
 $(document).ready(function(){
     getDeliveryOptions();
+    suplmenetingUserData()
 });
+const getID = () => {
+    return new Promise((resolve, reject) => {
+        let xmlhttp = new XMLHttpRequest();
+        let url = "../user_pages/get_data/get-id.php";
+        
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    let data = JSON.parse(this.responseText);
+                    resolve(data);
+                } else {
+                    reject(new Error('Request failed with status ' + this.status));
+                }
+            }
+        };
+
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    });
+}
+const suplmenetingUserData = async () =>{
+    let id = await getID()
+    const ajax = new XMLHttpRequest();
+    ajax.open("GET","../components/fetch-user-data.php" , true);
+    ajax.send();
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let database = JSON.parse(this.responseText);
+            for(let i = 0; i < database.length; i++){
+                if(database[i].ID_USER == id){
+                    name.value = `${database[i].imie_nazwisko}`
+                    address.value = `${database[i].adres}`
+                    email.value = `${database[i].email}`
+                    number.value = `${database[i].numer_telefonu}`
+                } 
+            }
+    }}
+}
+
+
 const getDeliveryId = () =>{
     const deliveryInputs = document.querySelectorAll("input[name=delivery]")
     deliveryInputs.forEach(input => {
