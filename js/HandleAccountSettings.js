ead503f807d43bae5183ebdb05cc0d2f838c53c7
Michał Kozak <live.m851@gmail.com>
2023-11-18 21:33:33 +0100

    Creating fucntion to show and exit popup with editing data, and complete data to inputs from database

--- js/HandleAccountSettings.js
+++ js/HandleAccountSettings.js
@@ -1,5 +1,11 @@
 const mainContent = document.querySelector(".main-content")
 const emailContent = document.querySelector(".email-content")
+
+const username = document.querySelector(".username")
+const firstName = document.querySelector(".first-name")
+const lastName = document.querySelector(".last-name")
+const address = document.querySelector(".address")
+
 const getID = () => {
     return new Promise((resolve, reject) => {
         let xmlhttp = new XMLHttpRequest();
@@ -34,9 +40,12 @@ const suplmenetingMainDataUser = async () =>{
                     <p><b>Nick:</b> ${database[i].username}</p>
                     <p><b>Imię:</b> ${database[i].imie}</p>
                     <p><b>Nazwisko:</b> ${database[i].nazwisko}</p>
-                    <p><b>Adres:</b> ${database[i].adres}</p>
-                `
+                    <p><b>Adres:</b> ${database[i].adres}</p>`
                     emailContent.innerHTML = `<p><b>Email:</b>  ${database[i].email}</p>`
+                    username.value = database[i].username
+                    firstName.value = database[i].imie
+                    lastName.value = database[i].nazwisko
+                    address.value = database[i].adres
                 } 
             }
     }}
@@ -49,3 +58,36 @@ const mainEdit = document.querySelector(".main-edit")
 const emailEdit = document.querySelector(".email-edit")
 const passwordEdit = document.querySelector(".password-edit")
 
+const editMainInfo = document.querySelector(".edit-main-info")
+const editEmailInfo = document.querySelector(".edit-email-info")
+const editPasswordInfo = document.querySelector(".edit-password-info")
+
+const exitMainInfo = document.querySelector(".exit-main")
+
+const hideElements = () => {
+    document.querySelector("#header").style.display = "none"
+    document.querySelector(".side-panel").style.display = "none"
+    document.querySelector(".client-dashboard").style.display = "none"
+}
+const showElements = () => {
+    document.querySelector("#header").style.display = "flex"
+    document.querySelector(".side-panel").style.display = "block"
+    document.querySelector(".client-dashboard").style.display = "flex"
+}
+
+
+const showEditMainInfo = () => {
+    hideElements()
+    editMainInfo.style.display = "flex"
+}
+const hideEditMainInfo = () => {
+    showElements()
+    editMainInfo.style.display = "none"
+}
+
+mainEdit.addEventListener("click",() => {
+    showEditMainInfo()
+})
+exitMainInfo.addEventListener("click",() => {
+    hideEditMainInfo()
+})
