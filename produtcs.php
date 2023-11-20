f3338f98050cfd2fe6d63204971b045806aaedfd
Michał Kozak <live.m851@gmail.com>
2023-11-20 21:56:36 +0100

    Create structure for input type range

--- produtcs.php
+++ produtcs.php
@@ -15,6 +15,20 @@
     <select class="category">
         
     </select>
+    <div class="price-range-container">
+        <label for="minPrice">Min Price:</label>
+        <input type="number" id="minPrice" min="0" step="1" value="0">
+        
+        <label for="maxPrice">Max Price:</label>
+        <input type="number" id="maxPrice" min="0" step="1" value="100">
+
+        <input type="range" id="priceSlider" min="0" max="100" value="0">
+
+        <div class="price-range-values">
+            <span id="minValue">0</span>
+            <span id="maxValue">100</span>
+        </div>
+    </div>
     </div>
     <div class="main">
     <?php include 'guest_pages/product-container.php'?>
