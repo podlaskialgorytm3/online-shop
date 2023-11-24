88d325647526df9951dcde0cbf5742e253ef21d8
Michał Kozak <live.m851@gmail.com>
2023-11-24 20:02:38 +0100

    Create a simple structure for search options

--- user_pages/products.php
+++ user_pages/products.php
@@ -14,9 +14,19 @@ if(!isset($_SESSION['user_name'])){
 </head>
 <body>
     <?php include 'components/header.php' ?>
+    <div class="search-options">
     <div class="container">
         <input type="text" placeholder="Parametr lub nazwa." id="search" >
         <div class="search"></div>
+    </div>
+    <select class="category">
+        
+    </select>
+    <div class="price-range-container">
+        <input type="number" class="price-input price-from" placeholder="od">
+        <span>---</span>
+        <input type="number" class="price-input price-to" placeholder="do">
+    </div>
     </div>
     <div class="main">
     <?php include '../user_pages/product-container.php'?>
